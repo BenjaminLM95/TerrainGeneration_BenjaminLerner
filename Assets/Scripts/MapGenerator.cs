23d3ac066e1a5ccd559473c2439e961433f31e34
BenjaminLM95 <benjamin.lerner95@gmail.com>
2025-12-02 23:39:02 -0400

    Adding list of regions

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -3,9 +3,6 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    public enum DrawMode { NoiseMap, ColorMap, Mesh};
-    public DrawMode drawMode;
-
     const int mapChunkSize = 241;
     [Range(0,6)]
     public int levelOfDetail;
@@ -23,14 +20,14 @@ public class MapGenerator : MonoBehaviour
     public float meshHeightMultiplier;
     public AnimationCurve meshHeightCurve; 
 
-    public bool autoUpdate;
+    public bool autoUpdate;    
 
-    public TerrainType[] regions; 
+    public RegionColors regionsColors; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        GenerateMap(); 
     }
 
     // Update is called once per frame
@@ -51,11 +48,11 @@ public class MapGenerator : MonoBehaviour
             {
                 float currentHeight = noiseMap[x, y];
 
-                for (int i = 0; i < regions.Length; i++) 
+                for (int i = 0; i < regionsColors.regions.Length; i++) 
                 {
-                    if(currentHeight <= regions[i].height) 
+                    if(currentHeight <= regionsColors.regions[i].height) 
                     {
-                        colorMap[y * mapChunkSize + x] = regions[i].color;
+                        colorMap[y * mapChunkSize + x] = regionsColors.regions[i].color;
                         break;
                     }
 
@@ -66,19 +63,9 @@ public class MapGenerator : MonoBehaviour
 
 
         PlaneTexture display = FindFirstObjectByType<PlaneTexture>();
-
-        if (drawMode == DrawMode.NoiseMap)
-        {
-            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
-        }
-        else if(drawMode == DrawMode.ColorMap) 
-        {
-            display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapChunkSize, mapChunkSize));
-        }
-        else if(drawMode == DrawMode.Mesh) 
-        {
-            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail), TextureGenerator.TextureFromColorMap(colorMap, mapChunkSize, mapChunkSize)); 
-        }
+              
+        display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail), TextureGenerator.TextureFromColorMap(colorMap, mapChunkSize, mapChunkSize)); 
+        
     }
 
     private void OnValidate()
---
+++ Assets/Scripts/RegionColors.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using static MapGenerator;
+
+public class RegionColors : MonoBehaviour
+{
+    public TerrainType[] regions = new TerrainType[8];
+
+
+    private void Awake()
+    {
+       
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
